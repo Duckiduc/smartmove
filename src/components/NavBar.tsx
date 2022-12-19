0692ac9e2a3c6bd20fe84858dc64dcbc8e0f2119
Duckiduc <git@ducki.dev>
2022-12-19 22:46:23 +0100

    Complete landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,45 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 
+const nav = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "1rem 2rem",
+  backgroundColor: "#fff",
+  position: "sticky",
+}
+
+const left = {
+  display: "flex",
+  alignItems: "center",
+}
+
+const logo = {
+  width: "15rem",
+}
+
+const link = {
+  marginLeft: "2rem",
+  color: "#071D3B",
+  textDecoration: "none",
+  fontFamily: "Arial, Helvetica, sans-serif",
+  fontSize: "1.2rem",
+  fontWeight: "bold",
+}
+
 export const NavBar = () => {
   return (
-    <nav>
-      <section>
-        <Link to="/">
+    <nav style={nav}>
+      <section style={left}>
+        <Link to="/" style={logo}>
           <StaticImage src="../images/smartmove-logo.png" alt="" />
         </Link>
-        <Link to="/calculator">Calculator</Link>
-        <Link to="/awareness">Awareness</Link>
+        <Link to="/calculator" style={link}>Calculator</Link>
+        <Link to="/awareness" style={link}>Awareness</Link>
       </section>
       <section>
-        <Link to="https://www.free-now.com/">FREENOW</Link>
+        <Link to="https://www.free-now.com/" style={link}>FREENOW</Link>
       </section>
     </nav>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,146 +1,171 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import { NavBar } from "../components/NavBar"
 
-// const pageStyles = {
-//   color: "#232129",
-//   padding: 96,
-//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// }
-// const headingStyles = {
-//   marginTop: 0,
-//   marginBottom: 64,
-//   maxWidth: 320,
-// }
-// const headingAccentStyles = {
-//   color: "#663399",
-// }
-// const paragraphStyles = {
-//   marginBottom: 48,
-// }
-// const codeStyles = {
-//   color: "#8A6534",
-//   padding: 4,
-//   backgroundColor: "#FFF4DB",
-//   fontSize: "1.25rem",
-//   borderRadius: 4,
-// }
-// const listStyles = {
-//   marginBottom: 96,
-//   paddingLeft: 0,
-// }
-// const doclistStyles = {
-//   paddingLeft: 0,
-// }
-// const listItemStyles = {
-//   fontWeight: 300,
-//   fontSize: 24,
-//   maxWidth: 560,
-//   marginBottom: 30,
-// }
+const moveAround = {
+  position: "relative",
+  display: "flex",
+  height: "40vh",
+}
 
-// const linkStyle = {
-//   color: "#8954A8",
-//   fontWeight: "bold",
-//   fontSize: 16,
-//   verticalAlign: "5%",
-// }
+const background = {
+  position: "absolute",
+  width: "100%",
+  height: "40vh",
+  objectFit: "cover",
+  zIndex: -1,
+  top: 0,
+  left: 0,
+}
 
-// const docLinkStyle = {
-//   ...linkStyle,
-//   listStyleType: "none",
-//   display: `inline-block`,
-//   marginBottom: 24,
-//   marginRight: 12,
-// }
+const backgroundDescriptor = {
+  backgroundColor: "#071D3B",
+  maxWidth: "33%",
+  color: "#FFF",
+  fontFamily: "Arial, Helvetica, sans-serif",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "3rem",
+}
 
-// const descriptionStyle = {
-//   color: "#232129",
-//   fontSize: 14,
-//   marginTop: 10,
-//   marginBottom: 0,
-//   lineHeight: 1.25,
-// }
+const conceptsTitle = {
+  fontFamily: "Arial, Helvetica, sans-serif",
+  marginTop: "2rem",
+}
 
-// const docLinks = [
-//   {
-//     text: "TypeScript Documentation",
-//     url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-//     color: "#8954A8",
-//   },
-//   {
-//     text: "GraphQL Typegen Documentation",
-//     url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-//     color: "#8954A8",
-//   }
-// ]
+const description = {
+  padding: "2rem 10rem",
+  fontFamily: "Arial, Helvetica, sans-serif",
+  lineHeight: "1.5rem",
+}
 
-// const badgeStyle = {
-//   color: "#fff",
-//   backgroundColor: "#088413",
-//   border: "1px solid #088413",
-//   fontSize: 11,
-//   fontWeight: "bold",
-//   letterSpacing: 1,
-//   borderRadius: 4,
-//   padding: "4px 6px",
-//   display: "inline-block",
-//   position: "relative" as "relative",
-//   top: -2,
-//   marginLeft: 10,
-//   lineHeight: 1,
-// }
+const calculatorButton = {
+  textDecoration: "none",
+  color: "#096BDB",
+  backgroundColor: "#FFF",
+  padding: ".8rem",
+  display: "inline-block",
+  borderRadius: "5px",
+}
 
-const links = [
+const conceptSection = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  paddingTop: "2rem",
+}
+
+const conceptArrow = {
+  width: "50px",
+}
+
+const concepts = {
+  display: "flex",
+}
+
+const concept = {
+  padding: "2rem",
+  paddingTop: "0",
+  fontFamily: "Arial, Helvetica, sans-serif",
+  maxWidth: "34%",
+  lineHeight: "1.5rem",
+}
+
+const conceptTitle = {
+  textAlign: "center",
+}
+
+const owningSection = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "#FF0A2B",
+  padding: "2rem",
+  fontFamily: "Arial, Helvetica, sans-serif",
+  color: "#FFF",
+}
+
+const owningDescription = {
+  maxWidth: "80%",
+  textAlign: "center",
+  fontSize: "1.3rem",
+  lineHeight: "2rem",
+}
+
+const texts = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    title: "MOVE AROUND...",
+    description: "Smart move allows you to estimate the cost (financial and ecological) of acquiring a car per year and suggests alternatives."
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    title: "Financial cost",
+    description: "Smart move allows to establish the financial cost of the acquisition and maintenance of a personal car. We use city statistics to get an estimate. We only need: the price of the car and its insurance and the price of the parking & the type of gasoline of the car as well as an estimation of the mileage with the consumption"
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+    title: "Ecological cost",
+    description: "Smart move allows to establish the ecological cost of the acquisition and the use of a car. We base our calculations on average CO2 emissions. We only need: the type of gasoline of the car as well as an estimation of the mileage with the consumption & the CO2 emission rate"
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+    title: "Alternatives",
+    description: "There are many alternatives to the car. Smart move recommends the best alternative means of transportation for your use. And the information you have entered. Smart move tries to offer you as many alternatives as possible so that you can make the best possible choice."
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
+    title: "OWNING A CAR IS NOT CHEAP.",
+    description: "Owning a car is a big expense. It generates a number of costs that we do not imagine (insurance, maintenance, parking ...). In general we are unable to estimate it. That's why Smart move exists and helps you find the best possible alternative."
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+    title: "",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. Integer in felis sed leo vestibulum venenatis. Suspendisse quis arcu sem. Aenean feugiat ex eu vestibulum vestibulum. Morbi a eleifend magna. Nam metus lacus, porttitor eu mauris a, blandit ultrices nibh. Mauris sit amet magna non ligula vestibulum eleifend. Nulla varius volutpat turpis sed lacinia. Nam eget mi in purus lobortis eleifend. Sed nec ante dictum sem condimentum ullamcorper quis venenatis nisi. Proin vitae facilisis nisi, ac posuere leo."
+  }
 ]
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <NavBar />
+      <section style={moveAround}>
+        <StaticImage src="../images/city.png" alt="" style={background} />
+        <div style={backgroundDescriptor}>
+          <div>
+            <h1>{texts[0].title}</h1>
+            <p>{texts[0].description}</p>
+            <Link to="/calculator" style={calculatorButton}>
+              ACCESS THE CALCULATOR
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section style={conceptSection}>
+        <StaticImage src="../images/arrow-down.png" alt="" style={conceptArrow}/>
+        <h1 style={conceptsTitle}>THE CONCEPT</h1>
+        <div style={concepts}>
+          <div style={concept}>
+            <h2 style={conceptTitle}>{texts[1].title}</h2>
+            <p>{texts[1].description}</p>
+          </div>
+          <div style={concept}>
+            <h2 style={conceptTitle}>{texts[2].title}</h2>
+            <p>{texts[2].description}</p>
+          </div>
+          <div style={concept}>
+            <h2 style={conceptTitle}>{texts[3].title}</h2>
+            <p>{texts[3].description}</p>
+          </div>
+        </div>
+      </section>
+      <section style={owningSection}>
+        <h1>{texts[4].title}</h1>
+        <p style={owningDescription}>{texts[4].description}</p>
+      </section>
+      <section style={description}>
+        <p>{texts[5].description}</p>
+      </section>
+      <footer>
+        <StaticImage src="../images/footer.png" alt="" />
+      </footer>
     </main>
   )
 }
